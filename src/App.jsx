f1445b2c420642c94133a9e16bfdce728f71587e
RomiRao <raoromina96@gmail.com>
2024-08-21 16:43:40 -0300

    set provisional navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Box
+          height="100vh"
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+        >
+          <Navbar />
+          <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            {/* <Route path="/products" element={} /> */}
+            {/* <Route path="/contact" element={} /> */}
+
+            {/* <Route path="/*" element={<ErrorPage />} /> */}
+          </Routes>
+        </Box>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Navbar/NavListDrawer.jsx
@@ -0,0 +1,33 @@
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
+import styles from "./NavListDrawer.module.css";
+
+export default function NavListDrawer({ setOpen, navLinks }) {
+  return (
+    <Box sx={{ width: 280 }} component="nav" bgcolor="black" height="100%">
+      <List>
+        {navLinks.map((link) => (
+          <NavLink
+            key={link}
+            to={`/${link !== "Home" ? link.toLowerCase() : ""}`}
+            className={({ isActive }) =>
+              isActive ? `${styles.active}` : `${styles.inactive}`
+            }
+          >
+            <ListItem disablePadding>
+              <ListItemButton sx={{ height: "70px" }}>
+                <ListItemText primary={link} />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+        ))}
+      </List>
+    </Box>
+  );
+}
