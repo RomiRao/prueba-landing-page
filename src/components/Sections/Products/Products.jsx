8d188561aedc5a88649f0e9f01b391672f8ebf13
RomiRao <raoromina96@gmail.com>
2024-08-21 19:43:59 -0300

    products structure done

--- src/components/Sections/Products/Products.jsx
+++ src/components/Sections/Products/Products.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import useFood from "../../../hooks/useFood";
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import ProductSkeleton from "./ProductSkeleton";
+import ProductCard from "./ProductCard";
 
 export default function Products() {
   const { obtData, ramen } = useFood();
@@ -10,31 +11,42 @@ export default function Products() {
   }, []);
   return (
     <>
-      <h1>Products</h1>
+      <Typography variant="h3" textAlign="center" marginY={4}>
+        Our Menu
+      </Typography>
       <Box display="flex" alignItems="center" flexDirection="column">
-        <Grid
-          container
-          maxWidth="100%"
-          justifyContent="center"
-          flexWrap="wrap"
-          marginY={10}
-        >
-          <ProductSkeleton />
-          <ProductSkeleton />
-          <ProductSkeleton />
-        </Grid>
+        {ramen.length === 0 ? (
+          <Grid
+            container
+            maxWidth="100%"
+            justifyContent="center"
+            flexWrap="wrap"
+            marginBottom={10}
+          >
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+          </Grid>
+        ) : (
+          <Grid
+            container
+            maxWidth="100%"
+            justifyContent="center"
+            flexWrap="wrap"
+            marginBottom={10}
+          >
+            {ramen.map((ramen) => (
+              <ProductCard
+                key={ramen.id}
+                name={ramen.name}
+                description={ramen.description}
+                price={ramen.price}
+                img={ramen.img}
+              />
+            ))}
+          </Grid>
+        )}
       </Box>
-      {/* {ramen.length === 0 ? (
-        <Box></Box>
-      ) : (
-        <Grid container>
-          {ramen.map((ramen) => (
-            <Grid item key={ramen.id} xs={3} padding={3}>
-              <Typography>{ramen.name}</Typography>
-            </Grid>
-          ))}
-        </Grid>
-      )} */}
     </>
   );
 }
