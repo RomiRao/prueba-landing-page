f70a46c3da68ec18de4b45ded3434b16b1d098f7
RomiRao <raoromina96@gmail.com>
2024-08-22 09:57:59 -0300

    menu design done, with fucking responsive

--- src/components/Sections/Products/ProductCard.jsx
+++ src/components/Sections/Products/ProductCard.jsx
@@ -1,42 +1,49 @@
-import { Box, Grid, Skeleton, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 export default function ProductCard({ name, description, price, img }) {
   return (
     <Grid
-      xs={8}
-      md={4}
-      lg={3}
       item
+      xs={12}
+      sm={6}
+      lg={4}
       display="flex"
-      justifyContent="center"
-      maxHeight={300}
-      margin={5}
+      minHeight={200}
+      marginY={2}
+      flexWrap={{ xs: "wrap", md: "nowrap" }}
     >
       <Box
         width="100%"
-        minWidth="150px"
-        height="100%"
-        maxHeight={360}
+        height={{ xs: "200px", md: "100%" }}
+        maxWidth={{ md: "140px" }}
+        marginRight={2}
         sx={{
-          marginRight: 5,
+          marginBottom: 2,
           backgroundImage: `url(${img})`,
           backgroundPosition: "center",
           backgroundSize: "cover",
+          borderRadius: 2,
+          boxShadow: "0px 0px 24px rgba(0,0,0,0.22)",
         }}
       />
       <Box
         display="flex"
         flexDirection="column"
         justifyContent="space-between"
-        minHeight="200px"
+        width="100%"
+        height="auto"
       >
         <Box>
-          <Typography sx={{ fontSize: "1rem", marginBottom: 3 }}>
+          <Typography fontSize="1rem" marginBottom={3} fontWeight={600}>
             {name}
           </Typography>
-          <Typography variant="body">{description}</Typography>
+          <Typography fontSize="0.85em" paddingRight={2} marginBottom={3}>
+            {description}
+          </Typography>
         </Box>
-        <Typography variant="rounded">{price}</Typography>
+        <Typography fontSize="1.5em" fontWeight={600}>
+          $ {price}
+        </Typography>
       </Box>
     </Grid>
   );
--- src/components/Sections/Products/Products.jsx
+++ src/components/Sections/Products/Products.jsx
@@ -3,50 +3,49 @@ import useFood from "../../../hooks/useFood";
 import { Box, Grid, Typography } from "@mui/material";
 import ProductSkeleton from "./ProductSkeleton";
 import ProductCard from "./ProductCard";
+import MenuBookRoundedIcon from "@mui/icons-material/MenuBookRounded";
 
 export default function Products() {
   const { obtData, ramen } = useFood();
+
   useEffect(() => {
     obtData();
   }, []);
+
   return (
-    <>
-      <Typography variant="h3" textAlign="center" marginY={4}>
-        Our Menu
-      </Typography>
-      <Box display="flex" alignItems="center" flexDirection="column">
-        {ramen.length === 0 ? (
-          <Grid
-            container
-            maxWidth="100%"
-            justifyContent="center"
-            flexWrap="wrap"
-            marginBottom={10}
-          >
-            <ProductSkeleton />
-            <ProductSkeleton />
-            <ProductSkeleton />
-          </Grid>
-        ) : (
-          <Grid
-            container
-            maxWidth="100%"
-            justifyContent="center"
-            flexWrap="wrap"
-            marginBottom={10}
-          >
-            {ramen.map((ramen) => (
-              <ProductCard
-                key={ramen.id}
-                name={ramen.name}
-                description={ramen.description}
-                price={ramen.price}
-                img={ramen.img}
-              />
-            ))}
-          </Grid>
-        )}
+    <Box paddingX={{ xs: 3, sm: 10 }}>
+      <Box display="flex" alignItems="center">
+        <Typography
+          component="h2"
+          marginY={4}
+          marginRight={2}
+          fontSize="2em"
+          fontWeight={500}
+          sx={{ color: "#222222" }}
+        >
+          Menu
+        </Typography>
+        <MenuBookRoundedIcon sx={{ fontSize: "2em", color: "#222222" }} />
       </Box>
-    </>
+      <Grid container flexWrap="wrap" marginBottom={10} spacing={2}>
+        {ramen.length === 0 ? (
+          <>
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+          </>
+        ) : (
+          ramen.map((ramen) => (
+            <ProductCard
+              key={ramen.id}
+              name={ramen.name}
+              description={ramen.description}
+              price={ramen.price}
+              img={ramen.img}
+            />
+          ))
+        )}
+      </Grid>
+    </Box>
   );
 }
